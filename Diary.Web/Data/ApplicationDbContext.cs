45d59b39aee0d7a2d2f9c2ab4ed072dbe335ccdb
Mix2511 <47683500+Mix2511@users.noreply.github.com>
2021-05-06 13:05:13 +0500

    Initial commit

---
+++ Diary.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Diary.Web.Data
+{
+    public class Teacher
+    {
+        public int Id { get; set; }
+        public string Post { get; set; }
+        public string Subject { get; set; }
+    }
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Grade { get; set; }
+    }
+    
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public DbSet<Teacher> Teachers { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            
+        }
+    }
+    public class ApplicationUser : IdentityUser
+    {
+        [Required()]
+        public string FirstName { get; set; }
+        [Required()]
+        public string LastName { get; set; }
+        [Required()]
+        public string MiddleName { get; set; }
+
+    }
+}
---
+++ Diary/Data/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Diary.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            
+        }
+    }
+
+    public class ApplicationUser : IdentityUser
+    {
+        [Required()]
+        public string FirstName { get; set; }
+        [Required()]
+        public string LastName { get; set; }
+        [Required()]
+        public string MiddleName { get; set; }
+    }
+}
